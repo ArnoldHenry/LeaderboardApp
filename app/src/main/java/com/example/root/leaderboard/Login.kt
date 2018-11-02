e87bc7e2d52b94129a1837dc855424f8de8460e6
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-11-02 17:23:46 +0300

    added butterknife for activity

--- app/src/main/java/com/example/root/leaderboard/Login.kt
+++ app/src/main/java/com/example/root/leaderboard/Login.kt
@@ -2,11 +2,28 @@ package com.example.root.leaderboard
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import butterknife.BindView
+import android.widget.EditText
+import butterknife.ButterKnife
+import butterknife.ButterKnife.bind
+
 
 class Login : AppCompatActivity() {
+    @BindView(R.id.buttonlogin)
+    var loginbtn: Button? = null
+
+    @BindView(R.id.usernameeditxt)
+    var user: EditText? = null
+
+    @BindView(R.id.passedittxt)
+    var pass: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        ButterKnife.bind(this)
+
     }
 }
